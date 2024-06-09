695a9faddb501a7e0d7108c3a4b5161ee987c39a
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-06-09 15:26:21 +0900

    add position entity

---
+++ entity/position.go
@@ -0,0 +1,80 @@
+package entity
+
+import (
+	"fmt"
+	"math"
+)
+
+type IPosition interface {
+	GetNext() (*PositionEntity, error)
+	GetX() int
+	GetY() int
+}
+
+type PositionEntity struct {
+	X int
+	Y int
+}
+
+func NewPositionEntity(x int, y int) IPosition {
+	return &PositionEntity{
+		X: x,
+		Y: y,
+	}
+}
+
+func abs(a int) int {
+	return int(math.Abs(float64(a)))
+}
+
+func (p *PositionEntity) GetNext() (*PositionEntity, error) {
+	loopNum := max(abs(p.X), abs(p.Y)) // NOTE: n周回
+
+	switch {
+	case isOnBottomSide(p.X, p.Y, loopNum):
+		return &PositionEntity{
+			X: p.X + 1,
+			Y: p.Y,
+		}, nil
+	case isOnRightSide(p.X, p.Y, loopNum):
+		return &PositionEntity{
+			X: p.X,
+			Y: p.Y + 1,
+		}, nil
+	case isOnTopSide(p.X, p.Y, loopNum):
+		return &PositionEntity{
+			X: p.X - 1,
+			Y: p.Y,
+		}, nil
+	case isOnLeftSide(p.X, p.Y, loopNum):
+		return &PositionEntity{
+			X: p.X,
+			Y: p.Y - 1,
+		}, nil
+	default:
+		return nil, fmt.Errorf("invalid position error")
+	}
+}
+
+func isOnRightSide(x int, y int, loopNum int) bool {
+	return x == loopNum && y < loopNum
+}
+
+func isOnTopSide(x int, y int, loopNum int) bool {
+	return -1*loopNum < x && y == loopNum
+}
+func isOnLeftSide(x int, y int, loopNum int) bool { //nolint:unparam
+	// y is not used in this function, but we want to keep the signature consistent
+	return x == -1*loopNum
+}
+func isOnBottomSide(x int, y int, loopNum int) bool {
+	return -1*loopNum < x && x < loopNum && y == -1*loopNum
+}
+
+func (p *PositionEntity) GetX() int {
+	return p.X
+}
+
+func (p *PositionEntity) GetY() int {
+	return p.Y
+}
