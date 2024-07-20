71ff04cbb98b956e34a02900f9aa1de22501c6fc
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-07-21 01:22:57 +0900

    update repository

--- domain/repository/drawing.go
+++ domain/repository/drawing.go
@@ -1,7 +1,12 @@
 package repository
 
-import "time"
+import (
+	"time"
+)
 
 type DrawingRepository interface {
 	GenerateSignedUrl(drawingName string, expiry time.Duration, method string) (string, error)
+	DownloadDrawing(url string) (data []byte, err error)
+	UploadDrawing(fileName string, fileData []byte) (string, error)
+	GenerateAIDrawing(drawingData map[string][]byte) ([]byte, error)
 }
